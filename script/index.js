7b60d33e26cc62839ee222f2c24b206a3ca49daa
Roy <roy.felix1609@gmail.com>
2022-07-05 21:04:39 +1000

    Better header

--- script/index.js
+++ script/index.js
@@ -1,7 +1,9 @@
+// Include header HTML file
 $(document).ready(function () {
     $('#header').load('header.html');
 });
 
-// $(function () {
-//     $('header').load('header.html');
-// });
+// Include main-page html file
+$(document).ready(function () {
+    $('#main-page').load('main-page.html');
+});
