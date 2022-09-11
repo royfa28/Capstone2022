cc5d7bf7765bc2f225cea90a6af884b0843e3aa7
Roy <roy.felix1609@gmail.com>
2022-09-11 22:01:43 +1000

    Make templates for order history and the page

--- src/App.js
+++ src/App.js
@@ -9,21 +9,23 @@ import ProductList from './components/body-comp/ProductList';
 import AccountPage from './components/account-comp/AccountPage';
 import ProductPage from './components/body-comp/ProductPage';
 import AccDetailsPage from "./components/account-comp/AccountDetailsPage";
+import OrderHistoryPage from './components/account-comp/OrderHistoryPage';
 
 const App = () => {
-  return (
-    <Container fluid="lg">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/Product-List/:consoleType" element={<ProductList />} />
-        <Route path="/Account_Page" element={<AccountPage />} >
-          <Route path="AccDetailsPage/:userID" element={<AccDetailsPage />} />
-        </Route>
-        <Route path="/ProductPage/:productID" element={<ProductPage />} />
-      </Routes>
-    </Container>
-  )
+    return (
+        <Container fluid="lg">
+            <Header />
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/Product-List/:consoleType" element={<ProductList />} />
+                <Route path="/Account_Page" element={<AccountPage />} >
+                    <Route path="Account_Page/:userID" element={<AccDetailsPage />} />  
+                </Route>
+                <Route path="Account_Page/Order_History/:orderID" element={<OrderHistoryPage />} />
+                <Route path="/ProductPage/:productID" element={<ProductPage />} />
+            </Routes>
+        </Container>
+    )
 }
 
 export default App
