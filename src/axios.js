2b5007a17941d8a3f56b6a23108b764106c25f17
Roy <roy.felix1609@gmail.com>
2022-09-23 23:43:45 +1000

    Baseurl change

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create(
-    {baseURL: "https://54.206.59.240/"}
+    {baseURL: "https://ec2-54-206-59-240.ap-southeast-2.compute.amazonaws.com/"}
 )
 
 export default instance;
