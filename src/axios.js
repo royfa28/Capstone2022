c31bfd8c9cc426594fc47e7b34aac4c62c6628cf
Roy <roy.felix1609@gmail.com>
2022-09-24 11:28:26 +1000

    Change to http and working on axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create(
     {
-        baseURL: "https://54.206.59.240:8080/"
+        baseURL: "http://ec2-54-206-59-240.ap-southeast-2.compute.amazonaws.com:8080/"
     }
 )
 
